2c3dfa34f78865f58cf5a4c04caa681167d63e4e
griechischlernenapp <larchanidis@pm.me>
2026-09-13 12:49:02 +0200

    polyphem story

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // Griechisch Lernen — Service Worker
 // Version hochzählen bei jedem Update auf GitHub!
-var CACHE_NAME = 'griechisch-lernen-v14';
+var CACHE_NAME = 'griechisch-lernen-v15';
 
 var STATIC_FILES = [
   '/griechisch-lernen/',
