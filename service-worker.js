a5697867e5fdeb6d5dd371e377973a292c776be4
griechischlernenapp <larchanidis@pm.me>
2026-09-15 18:32:32 +0200

    hochladen

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // Griechisch Lernen — Service Worker
 // Version hochzählen bei jedem Update auf GitHub!
-var CACHE_NAME = 'griechisch-lernen-v28';
+var CACHE_NAME = 'griechisch-lernen-v29';
 
 var STATIC_FILES = [
   '/griechisch-lernen/',
