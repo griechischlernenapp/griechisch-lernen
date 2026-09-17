700ae69be46199c621a1772802c331351ada739d
griechischlernenapp <larchanidis@pm.me>
2026-09-17 23:48:52 +0200

    tonplan

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // Griechisch Lernen — Service Worker
 // Version hochzählen bei jedem Update auf GitHub!
-var CACHE_NAME = 'griechisch-lernen-v37';
+var CACHE_NAME = 'griechisch-lernen-v38';
 
 var STATIC_FILES = [
   '/griechisch-lernen/',
