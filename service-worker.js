f581b529ea711ea53d7993208137cac68e71be10
griechischlernenapp <larchanidis@pm.me>
2026-09-17 10:08:34 +0200

    kapitel 16

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // Griechisch Lernen — Service Worker
 // Version hochzählen bei jedem Update auf GitHub!
-var CACHE_NAME = 'griechisch-lernen-v34';
+var CACHE_NAME = 'griechisch-lernen-v35';
 
 var STATIC_FILES = [
   '/griechisch-lernen/',
